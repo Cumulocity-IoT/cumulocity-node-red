91ed29037c3d0f1f49d4ce684020bb16cfa10f75
Tristan Bastian <tristan.bastian@cumulocity.com>
2025-03-13 12:05:44 +0100

    feat: upgrade Web SDK to 1021 (#246)

--- cumulocity-node-red-ui/src/main.ts
+++ cumulocity-node-red-ui/src/main.ts
@@ -2,17 +2,19 @@ import './i18n';
 import { applyOptions, loadOptions } from '@c8y/bootstrap';
 
 const barHolder: HTMLElement | null = document.querySelector('body > .init-load');
-export const removeProgress = () => barHolder && barHolder.parentNode?.removeChild(barHolder);
+export const removeProgress = () => barHolder?.parentNode?.removeChild(barHolder);
 
 applicationSetup();
 
 async function applicationSetup() {
-  const options = await applyOptions({
+  await applyOptions({
     ...(await loadOptions())
   });
 
-  const mod = await import('./bootstrap');
-  const bootstrapApp = mod.bootstrap || (window as any).bootstrap || (() => null);
-
-  return Promise.resolve(bootstrapApp()).then(removeProgress);
+  const mod = await import(
+    /* webpackPreload: true */
+    './bootstrap'
+  );
+  const bootstrapApp = mod.bootstrap;
+  return bootstrapApp().then(removeProgress);
 }
